e6cbdeb578a1618c8d18a751358491b6bc405361
Sarabesh-13 <sarabeshkanish@gmail.com>
2026-01-29 10:51:35 +0530

    updated day31 tasks.

--- Classwork/Day31/tcp_udp_applications/problem_2/app_client.cpp
+++ Classwork/Day31/tcp_udp_applications/problem_2/app_client.cpp
@@ -13,6 +13,7 @@ class VehicleClient {
 public:
     VehicleClient(int id) : vehicleId(id) {
         sock = socket(AF_INET, SOCK_DGRAM, 0);
+
         server.sin_family = AF_INET;
         server.sin_port = htons(9000);
         server.sin_addr.s_addr = inet_addr("127.0.0.1");
@@ -33,14 +34,22 @@ public:
             sendto(sock, buffer, strlen(buffer), 0,
                    (sockaddr*)&server, sizeof(server));
 
-            std::cout << "Sent: " << buffer << std::endl;
+            std::cout << "Vehicle " << vehicleId
+                      << " sent telemetry\n";
             sleep(1);
         }
     }
 };
 
-int main() {
-    VehicleClient client(101);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        std::cerr << "Usage: ./vehicle_client <vehicle_id>\n";
+        return 1;
+    }
+
+    int vehicleId = std::stoi(argv[1]);
+    VehicleClient client(vehicleId);
     client.sendTelemetry();
+
     return 0;
 }
