1e6b9458336e396739495181f03dd401f262e587
Sarabesh-13 <sarabeshkanish@gmail.com>
2026-01-31 11:50:46 +0530

    changed file names as per TOC tasks.

---
+++ Classwork/Day13(C++ Programming)/program2.c
@@ -0,0 +1,24 @@
+//program for file handling in c.
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define print(a,b) printf("%d %d",a,b)
+
+int main()
+{
+  FILE* fp=fopen("/home/student/programs/file.txt","r");
+  char str[20];
+  if(fp==NULL)
+  {
+    printf("file not found\n");
+    exit(EXIT_FAILURE);
+  }
+  memset(str,'\0',sizeof(str));
+  fscanf(fp,"%[^\n]",str);
+  fprintf(stdout,"\n%s\n",str);
+  fclose(fp);
+  return 0;
+}
+
